c5523d6d539e1a9d429fbf5eaa4926e0886ef067
jun <ggprgrkjh@naver.com>
2023-02-13 03:02:26 +0900

    feat: SignIn page and navigation

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:escort/page1.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -11,6 +12,242 @@ class MyApp extends StatefulWidget {
 
   @override
   State<MyApp> createState() => _MyAppState();
+}
+
+class SignIn extends StatelessWidget {
+  const SignIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+          backgroundColor: Colors.transparent,
+          bottomOpacity: 0.0,
+          elevation: 0.0,
+          leading: GestureDetector(
+            child: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 260, top: 20, bottom: 25),
+              child: Text("Sign In",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 110),
+              child: Text("Please enter Your ID and Password"),
+            ),
+            Container(
+              height: 60,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 325, bottom: 20),
+              child: Text(
+                "ID",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              width: 350,
+              child: TextFormField(
+                style: TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.black12,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 275, bottom: 20, top: 30),
+              child: Text(
+                "Password",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              width: 350,
+              child: TextFormField(
+                style: TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.black12,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 170.0,
+              ),
+              child: TextButton(
+                onPressed: () => {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => ResetPassword()))
+                },
+                child: Text(
+                  "Forgot password? Click here",
+                  style: TextStyle(fontSize: 12, color: Colors.blue),
+                ),
+              ),
+            )
+          ],
+        ),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SizedBox(
+            height: 50,
+            child: ElevatedButton(
+              onPressed: () => {},
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                backgroundColor: Color.fromRGBO(16, 64, 59, 10),
+                fixedSize: const Size(250.0, 40.0),
+              ),
+              child: Text("Continue"),
+            ),
+          ),
+        ));
+  }
+}
+
+class ResetPassword extends StatelessWidget {
+  const ResetPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+          backgroundColor: Colors.transparent,
+          bottomOpacity: 0.0,
+          elevation: 0.0,
+          leading: GestureDetector(
+            child: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 80, top: 20, bottom: 25),
+              child: Text("Create New Password",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+            ),
+            Padding(
+                padding: const EdgeInsets.only(right: 45),
+                child: Text("Enter your new password if you forget it,",
+                    style: TextStyle(fontSize: 16))),
+            Padding(
+              padding: const EdgeInsets.only(right: 70),
+              child: Text(
+                "then you have to reset the password.",
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            Container(
+              height: 60,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 280, bottom: 20),
+              child: Text(
+                "Password",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              width: 350,
+              child: TextFormField(
+                style: TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.black12,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 220, bottom: 20, top: 30),
+              child: Text(
+                "Confirm Password",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              width: 350,
+              child: TextFormField(
+                style: TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.black12,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SizedBox(
+            height: 50,
+            child: ElevatedButton(
+              onPressed: () => {},
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                backgroundColor: Color.fromRGBO(16, 64, 59, 10),
+                fixedSize: const Size(250.0, 40.0),
+              ),
+              child: Text("Continue"),
+            ),
+          ),
+        ));
+  }
 }
 
 class _MyAppState extends State<MyApp> {
@@ -90,7 +327,12 @@ class _MyAppState extends State<MyApp> {
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.all(Radius.circular(100))),
                       child: ElevatedButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SignIn()));
+                        },
                         style: ElevatedButton.styleFrom(
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(20.0),
