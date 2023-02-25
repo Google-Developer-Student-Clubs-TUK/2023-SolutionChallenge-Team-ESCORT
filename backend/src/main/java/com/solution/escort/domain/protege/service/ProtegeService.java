a6a5f0bb2bce76c1fbdbea711f84b2136b72d617
KimHyeonJin <hyeonjin9819@gmail.com>
2023-02-26 00:47:26 +0900

    feat: update protege's clothing api
    배회노인 신고 시 옷차림 업데이트 관련 API 완료

--- backend/src/main/java/com/solution/escort/domain/protege/service/ProtegeService.java
+++ backend/src/main/java/com/solution/escort/domain/protege/service/ProtegeService.java
@@ -1,5 +1,6 @@
 package com.solution.escort.domain.protege.service;
 
+import com.solution.escort.domain.protege.dto.request.ProtegeClothRequestDTO;
 import com.solution.escort.domain.protege.dto.request.ProtegeRequestDTO;
 import com.solution.escort.domain.protege.dto.response.ProtegeResponseDTO;
 
@@ -11,5 +12,8 @@ public interface ProtegeService {
 
     // 노인 한명의 정보 가져오기 관련 서비스
     public ProtegeResponseDTO getProtegeById(Integer id) throws Exception;
+
+    // 노인 신고 시 노인의 옷차림 추가하는 API
+    public void protegeCloth(ProtegeClothRequestDTO protegeClothRequestDTO, Integer id) throws Exception;
 
 }
