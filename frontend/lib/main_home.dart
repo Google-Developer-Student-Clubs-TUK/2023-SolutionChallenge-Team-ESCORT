5704e20f2c8436c47e5c05be5fbbf609a5aa0574
jun <ggprgrkjh@naver.com>
2023-03-19 03:38:37 +0900

    feat : dementia sos and  delete api connect

--- frontend/lib/main_home.dart
+++ frontend/lib/main_home.dart
@@ -58,6 +58,35 @@ class DementiaLocationTracker {
   }
 }
 
+void dementiaSos(dementiauid) async {
+  var headers = {'Content-Type': 'application/x-www-form-urlencoded'};
+  var request =
+      http.Request('POST', Uri.parse('http://34.22.70.120:8080/api/v1/sos'));
+  request.bodyFields = {'protegeUId': dementiauid};
+  request.headers.addAll(headers);
+
+  http.StreamedResponse response = await request.send();
+  if (response.statusCode == 201) {
+    print(await response.stream.bytesToString());
+  } else {
+    print(response.reasonPhrase);
+  }
+}
+
+void cancleSos(dementauid) async {
+  var request = http.Request('DELETE',
+      Uri.parse('http://34.22.70.120:8080/api/v1/protege/$dementauid'));
+  request.bodyFields = {};
+
+  http.StreamedResponse response = await request.send();
+
+  if (response.statusCode == 200) {
+    print(await response.stream.bytesToString());
+  } else {
+    print(response.reasonPhrase);
+  }
+}
+
 class MapSample extends StatefulWidget {
   const MapSample({Key? key}) : super(key: key);
 
@@ -374,7 +403,12 @@ class MapSampleState extends State<MapSample> {
                                                             .styleFrom(
                                                                 primary: Colors
                                                                     .black12),
-                                                        onPressed: () => {},
+                                                        onPressed: () => {
+                                                              Navigator.pop(
+                                                                  context), // 바텀 시트를 닫습니다.
+                                                              cancleSos(element[
+                                                                  'uid'])
+                                                            },
                                                         child: Container(
                                                             width: MediaQuery.of(
                                                                         context)
@@ -402,7 +436,11 @@ class MapSampleState extends State<MapSample> {
                                                                     59,
                                                                     10),
                                                           ),
-                                                          onPressed: () => {},
+                                                          onPressed: () => {
+                                                                dementiaSos(
+                                                                    element[
+                                                                        'uid'])
+                                                              },
                                                           child: Container(
                                                             width: MediaQuery.of(
                                                                         context)
