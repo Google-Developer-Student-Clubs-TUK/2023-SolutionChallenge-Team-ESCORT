a02d8430125c0a602f68a6397d7b57f40190b656
KimHyeonJin <hyeonjin9819@gmail.com>
2023-03-19 15:10:52 +0900

    fix: sos delete api
    신고 api 수정

--- backend/src/main/java/com/solution/escort/domain/sos/repository/SOSRepository.java
+++ backend/src/main/java/com/solution/escort/domain/sos/repository/SOSRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SOSRepository extends JpaRepository<SOS, Integer> {
     SOS findByProtegeId(int id);
+
+    boolean existsByProtegeId(int id);
+
 }
