db0853e9ea279e432e65d7e720406671d80d3cc9
Holy-Knight <vnycall74@naver.com>
2023-03-20 05:20:05 +0900

    feat: qr for register dementia

--- frontend/lib/main_registration_page.dart
+++ frontend/lib/main_registration_page.dart
@@ -346,121 +346,168 @@ class _QRViewExampleState extends State<QRViewExample> {
     setState(() {
       this.controller = controller;
     });
-    controller.scannedDataStream.listen((scanData) {
-      setState(() {
-        result = scanData;
+    controller.scannedDataStream.listen(
+      (scanData) {
+        setState(
+          () {
+            result = scanData;
+            String? code = result?.code;
 
-        String? code = result?.code;
+            controller.stopCamera();
 
-        controller.stopCamera();
+            if (code != null) {
+              _requestPostRegisterDementia(
+                code,
+                (onResult) {
+                  if (onResult) {
+                    resultDialog(
+                      Icons.check_box,
+                      'Scan Successful!',
+                      'The old man has been registered on the registration list.',
+                      'Go to Home',
+                      () {
+                        Navigator.pop(context);
+                        Get.back();
+                      },
+                    );
+                  } else {
+                    resultDialog(
+                      Icons.error,
+                      'Scan Failure!',
+                      'Invalid qr code or network error.',
+                      'Try again',
+                      () {
+                        Navigator.pop(context);
+                        controller.resumeCamera();
+                      },
+                    );
+                  }
+                },
+              );
+            } else {
+              resultDialog(
+                Icons.error,
+                'Scan Failure!',
+                'Invalid qr code.',
+                'Try again',
+                () {
+                  Navigator.pop(context);
+                  controller.resumeCamera();
+                },
+              );
+            }
+          },
+        );
+      },
+    );
+  }
 
-        print("PangMoo - code: $code");
-
-        if (code != null) {
-          Get.dialog(
-            Expanded(
-                child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 32),
-                child: Scaffold(
-                  backgroundColor: Colors.transparent,
-                  body: Center(
-                    child: Wrap(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(32),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.fromLTRB(16, 42, 16, 32),
-                            child: Column(
-                              children: [
-                                Container(
-                                  width: 112,
-                                  height: 112,
-                                  decoration: BoxDecoration(
-                                    gradient: LinearGradient(
-                                      colors: const [
-                                        Color(0xCC10403B),
-                                        Color(0xFF10403B)
-                                      ],
-                                    ),
-                                    shape: BoxShape.circle,
-                                  ),
-                                  child: Center(
-                                    child: Icon(
-                                      Icons.check_box,
-                                      size: 40,
-                                      color: Colors.white,
-                                    ),
-                                  ),
+  void resultDialog(
+    IconData icon,
+    String title,
+    String content,
+    String buttonText,
+    GestureTapCallback onClick,
+  ) {
+    Get.dialog(
+      Expanded(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 32),
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+              body: Center(
+                child: Wrap(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(32),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(16, 42, 16, 32),
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 112,
+                              height: 112,
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: const [
+                                    Color(0xCC10403B),
+                                    Color(0xFF10403B)
+                                  ],
                                 ),
-                                SizedBox(height: 24),
-                                Text(
-                                  'Scan Successful!',
-                                  style: TextStyle(
-                                    color: Color(0xFF212121),
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                                shape: BoxShape.circle,
+                              ),
+                              child: Center(
+                                child: Icon(
+                                  icon,
+                                  size: 40,
+                                  color: Colors.white,
                                 ),
-                                SizedBox(height: 18),
-                                Text(
-                                  'The old man has been registered on the registration list.',
-                                  style: TextStyle(
-                                    color: Color(0xFF212121),
-                                    fontSize: 16,
-                                  ),
-                                  textAlign: TextAlign.center,
-                                ),
-                                SizedBox(
-                                  height: 56,
-                                ),
-                                Material(
-                                  child: InkWell(
-                                    onTap: () {
-                                      print("WOW");
-                                    },
-                                    borderRadius: BorderRadius.circular(100),
-                                    child: Ink(
-                                      width: double.infinity,
-                                      decoration: BoxDecoration(
-                                        color: Color(0xFF10403B),
-                                        borderRadius:
-                                            BorderRadius.circular(100),
-                                      ),
-                                      child: Padding(
-                                        padding: EdgeInsets.all(16),
-                                        child: Text(
-                                          'Go to Home',
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                          textAlign: TextAlign.center,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
+                              ),
                             ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
+                            SizedBox(height: 24),
+                            Text(
+                              title,
+                              style: TextStyle(
+                                color: Color(0xFF212121),
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 18),
+                            Text(
+                              content,
+                              style: TextStyle(
+                                color: Color(0xFF212121),
+                                fontSize: 16,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(
+                              height: 56,
+                            ),
+                            Material(
+                              child: InkWell(
+                                onTap: onClick,
+                                borderRadius: BorderRadius.circular(100),
+                                child: Ink(
+                                  width: double.infinity,
+                                  decoration: BoxDecoration(
+                                    color: Color(0xFF10403B),
+                                    borderRadius: BorderRadius.circular(100),
+                                  ),
+                                  child: Padding(
+                                    padding: EdgeInsets.all(16),
+                                    child: Text(
+                                      buttonText,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
-            )),
-            barrierDismissible: false,
-          );
-        }
-      });
-    });
+            ),
+          ),
+        ),
+      ),
+      barrierDismissible: false,
+    );
   }
 
   void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
@@ -470,6 +517,20 @@ class _QRViewExampleState extends State<QRViewExample> {
         const SnackBar(content: Text('no Permission')),
       );
     }
+  }
+
+  void _requestPostRegisterDementia(String code, void Function(bool) onResult) {
+    GetConnect().post(
+      'http://34.22.70.120:8080/api/v1/ppConnection',
+      {
+        "protectorUId": FirebaseAuth.instance.currentUser?.uid ?? "-",
+        "protegeUId": code
+      },
+    ).then(
+      (value) {
+        onResult(value.body['code'] == 'PP000');
+      },
+    );
   }
 
   @override
