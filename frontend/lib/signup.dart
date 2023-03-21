790ef5a8d1fa4b72c31fe5ad7c8070d1a5be0862
jun <ggprgrkjh@naver.com>
2023-03-21 19:05:05 +0900

    feat: add signup success dialog

--- frontend/lib/signup.dart
+++ frontend/lib/signup.dart
@@ -7,9 +7,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animation_progress_bar/flutter_animation_progress_bar.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:image_picker/image_picker.dart';
-import 'firebase_auth.dart';
+
 import 'auth_controller.dart';
+import 'firebase_auth.dart';
 
 enum Dementia { yes, no }
 
@@ -111,7 +113,7 @@ class _SignUpState extends State<SignUp> {
               ),
               RadioListTile(
                   activeColor: Color.fromRGBO(16, 64, 59, 10),
-                  title: Text("Yes, But I'm good"),
+                  title: Text("Yes"),
                   value: Dementia.yes,
                   groupValue: _dementia,
                   onChanged: (value) {
@@ -129,7 +131,7 @@ class _SignUpState extends State<SignUp> {
               ),
               RadioListTile(
                   activeColor: Color.fromRGBO(16, 64, 59, 10),
-                  title: Text("No, I'm good"),
+                  title: Text("No"),
                   value: Dementia.no,
                   groupValue: _dementia,
                   onChanged: (value) {
@@ -526,142 +528,138 @@ class _SignUpState4 extends State<SignUp4> {
         ),
         body: SingleChildScrollView(
           scrollDirection: Axis.vertical,
-          child: ListView(children: [
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(
-                      right: 100, top: 20, bottom: 25, left: 15),
-                  child: Text("Give Us More Detail About You!",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    right: 100, top: 20, bottom: 25, left: 15),
+                child: Text("Give Us More Detail About You!",
+                    style:
+                        TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 80),
+                child: Text("The information is necessary to use our"),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 290),
+                child: Text("service."),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 270, top: 15),
+                child: Text(
+                  "Your Image",
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 80),
-                  child: Text("The information is necessary to use our"),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 290),
-                  child: Text("service."),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 270, top: 15),
-                  child: Text(
-                    "Your Image",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                if (_pickedFile == null)
-                  Container(
-                      constraints: BoxConstraints(
-                        minHeight: imageSize,
-                        minWidth: imageSize,
-                      ),
-                      child: GestureDetector(
-                          onTap: () {
-                            _showBottomSheet();
-                          },
-                          child: Stack(
-                            children: [
-                              Image.asset(
-                                "assets/profileimage.png",
-                                width: imageSize,
-                                height: imageSize,
-                              ),
-                              Padding(
-                                padding:
-                                    const EdgeInsets.only(left: 70, top: 70),
-                                child: Image.asset(
-                                  "assets/imageplus.png",
-                                  width: 30,
-                                  height: 30,
-                                ),
-                              )
-                            ],
-                          )))
-                else
-                  Center(
-                    child: Container(
-                      width: imageSize,
-                      height: imageSize,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                            image: FileImage(_pickedFile), fit: BoxFit.cover),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 70, top: 65),
-                        child: Image.asset(
-                          "assets/imageplus.png",
-                          width: 20,
-                          height: 20,
-                        ),
-                      ),
-                    ),
-                  ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              if (_pickedFile == null)
                 Container(
-                  height: 25,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 235, bottom: 20),
-                  child: Text(
-                    "Characteristics",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-                SizedBox(
-                  width: 350,
-                  height: 45,
-                  child: TextFormField(
-                    style: TextStyle(color: Colors.black),
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.black12,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: BorderSide(
-                          width: 0,
-                          style: BorderStyle.none,
-                        ),
+                    constraints: BoxConstraints(
+                      minHeight: imageSize,
+                      minWidth: imageSize,
+                    ),
+                    child: GestureDetector(
+                        onTap: () {
+                          _showBottomSheet();
+                        },
+                        child: Stack(
+                          children: [
+                            Image.asset(
+                              "assets/profileimage.png",
+                              width: imageSize,
+                              height: imageSize,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 70, top: 70),
+                              child: Image.asset(
+                                "assets/imageplus.png",
+                                width: 30,
+                                height: 30,
+                              ),
+                            )
+                          ],
+                        )))
+              else
+                Center(
+                  child: Container(
+                    width: imageSize,
+                    height: imageSize,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: FileImage(_pickedFile), fit: BoxFit.cover),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 70, top: 65),
+                      child: Image.asset(
+                        "assets/imageplus.png",
+                        width: 20,
+                        height: 20,
                       ),
                     ),
-                    onChanged: (value) =>
-                        {authController.setCharacteristics(value)},
                   ),
                 ),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(right: 265, bottom: 20, top: 20),
-                  child: Text(
-                    "Blood Type",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
+              Container(
+                height: 25,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 235, bottom: 20),
+                child: Text(
+                  "Characteristics",
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-                SizedBox(
-                  width: 350,
-                  height: 45,
-                  child: TextFormField(
-                    style: TextStyle(color: Colors.black),
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.black12,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: BorderSide(
-                          width: 0,
-                          style: BorderStyle.none,
-                        ),
+              ),
+              SizedBox(
+                width: 350,
+                height: 45,
+                child: TextFormField(
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.black12,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide(
+                        width: 0,
+                        style: BorderStyle.none,
                       ),
                     ),
-                    onChanged: (value) => {authController.setBlood(value)},
                   ),
+                  onChanged: (value) =>
+                      {authController.setCharacteristics(value)},
                 ),
-              ],
-            ),
-          ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 265, bottom: 20, top: 20),
+                child: Text(
+                  "Blood Type",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                width: 350,
+                height: 45,
+                child: TextFormField(
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.black12,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide(
+                        width: 0,
+                        style: BorderStyle.none,
+                      ),
+                    ),
+                  ),
+                  onChanged: (value) => {authController.setBlood(value)},
+                ),
+              ),
+            ],
+          ),
         ),
         bottomNavigationBar: Padding(
           padding: const EdgeInsets.all(20.0),
@@ -996,3 +994,194 @@ class _SignUpState5 extends State<SignUp5> {
         ));
   }
 }
+
+
+/*
+class _SignUpState5 extends State<SignUp5> {
+  GoogleMapController? _controller;
+  Set<Marker> _markers = {};
+  LatLng _initialPosition = LatLng(37.4220, -122.0841);
+  String _address = "";
+
+  void _handleTap(LatLng latLng) async {
+    print("handletpa");
+    setState(() {
+      _markers.clear();
+      _markers.add(
+        Marker(
+          markerId: MarkerId(latLng.toString()),
+          position: latLng,
+        ),
+      );
+      _address = "Googleplex, Mountain View, CA 94043, United States";
+    });
+
+    if (_controller != null) {
+      _controller!.animateCamera(CameraUpdate.newCameraPosition(
+          CameraPosition(target: latLng, zoom: 15)));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: SizedBox(
+            width: 220,
+            height: 15,
+            child: FAProgressBar(
+              currentValue: 100,
+              progressColor: Color.fromRGBO(16, 64, 59, 10),
+              backgroundColor: Color.fromRGBO(16, 64, 59, 180),
+            ),
+          ),
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+          backgroundColor: Colors.transparent,
+          bottomOpacity: 0.0,
+          elevation: 0.0,
+          leading: GestureDetector(
+            child: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        body: Container(
+          width: 500,
+          height: 600,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    right: 50, top: 20, bottom: 25, left: 15),
+                child: Text("Set up Your safe zone!",
+                    style:
+                        TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 60),
+                child: Text("Search the address or move the map"),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 60.0),
+                child: Container(
+                  width: 400,
+                  height: 350,
+                  child: GoogleMap(
+                    initialCameraPosition:
+                        CameraPosition(target: _initialPosition, zoom: 15),
+                    markers: _markers,
+                    onTap: _handleTap,
+                    onMapCreated: (GoogleMapController controller) {
+                      _controller = controller;
+                    },
+                  ),
+                ),
+              ),
+              Container(
+                width: 400,
+                height: 50,
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Text(
+                  _address,
+                  style: TextStyle(fontSize: 13),
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SizedBox(
+            height: 50,
+            child: ElevatedButton(
+              onPressed: () => {
+                showDialog<void>(
+                  //다이얼로그 위젯 소환
+                  context: context,
+                  barrierDismissible: false, // 다이얼로그 이외의 바탕 눌러도 안꺼지도록 설정
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      content: SingleChildScrollView(
+                        child: ListBody(
+                          //List Body를 기준으로 Text 설정
+                          children: <Widget>[
+                            Center(
+                              child: Column(
+                                children: [
+                                  SizedBox(
+                                    height: 40,
+                                  ),
+                                  Image.asset("assets/signupmark.png"),
+                                  SizedBox(
+                                    height: 60,
+                                  ),
+                                  Text('Sign Up Successful!',
+                                      style: TextStyle(
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.bold)),
+                                  SizedBox(
+                                    height: 40,
+                                  ),
+                                  Text('Your account has been created'),
+                                  Text('Let\'s begin!'),
+                                  SizedBox(
+                                    height: 60,
+                                  ),
+                                  SizedBox(
+                                    width:
+                                        MediaQuery.of(context).size.width * 0.6,
+                                    height: MediaQuery.of(context).size.height *
+                                        0.05,
+                                    child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        primary: Color(0xCC10403B),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(30.0),
+                                        ),
+                                      ),
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) => MyApp(),
+                                            ));
+                                      },
+                                      child: Text('Go to Home'),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                )
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                backgroundColor: Color.fromRGBO(16, 64, 59, 10),
+                fixedSize: const Size(250.0, 40.0),
+              ),
+              child: Text("Continue"),
+            ),
+          ),
+        ));
+  }
+}
+
+*/
