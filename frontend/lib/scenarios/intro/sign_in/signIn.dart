bc03ff39b73cd053f66264ad42a8809b3d3cb51b
jun <ggprgrkjh@naver.com>
2023-05-19 23:01:20 +0900

    feat: password obscureText

--- frontend/lib/scenarios/intro/sign_in/signIn.dart
+++ frontend/lib/scenarios/intro/sign_in/signIn.dart
@@ -228,6 +228,7 @@ class SignIn extends StatelessWidget {
               SizedBox(
                 width: 350,
                 child: TextFormField(
+                  obscureText: true,
                   style: TextStyle(color: Colors.black),
                   decoration: InputDecoration(
                     filled: true,
